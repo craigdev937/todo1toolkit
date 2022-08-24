64977f87a02ba20d81dbf5f0658bdd929870948e
Craig <indigomx9@gmail.com>
2022-08-24 17:49:58 -0400

    Initial setup of the project.

---
+++ README.md
@@ -0,0 +1,6 @@
+# MERN 
+
+Initial setup of the project.  Created with React, Express, Mongoose, Type Modules, React Router, and Redux-Toolkit.
+
+
+
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import express from "express";
+
+(async () => {
+    const app = express();
+
+    app.get("/", (req, res) => res.json({
+        hello: "world!"
+    }));
+
+    const port = process.env.PORT;
+    app.listen(port, () => {
+        console.log(`Server: http://localhost:${port}`);
+        console.log("Press Ctrl + C to exit.");
+    })
+})();
+
+
+
